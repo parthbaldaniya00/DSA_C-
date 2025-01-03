804f170233351221de8e57116af53f74677d6a10
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2025-01-03 22:54:19 +0530

    Javascript DSA Question

---
+++ JavaScript DSA/twosum.js
@@ -0,0 +1,14 @@
+let twoSum = function(nums, target) {
+    let obj = {};
+
+    for(let i=0; i<nums.length; i++){
+        let n = nums[i];
+        if(obj[target-n] >= 0){
+            return [ obj[target-n], i];
+        }else{
+            obj[n] = i;
+        }
+    }
+};
+
+console.log(twoSum([2,7,11,15], 9));
